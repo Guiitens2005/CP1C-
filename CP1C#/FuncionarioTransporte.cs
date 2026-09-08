1c6d933ed9d6ae8da892409efa3fb9b8677a2a7e
Caseiro <guilherme.a.caseiro@gmail.com>
2026-09-08 15:55:46 -0300

    feat: criação da Classe Pai FuncionarioTransporte

---
+++ CP1C#/FuncionarioTransporte.cs
@@ -0,0 +1,22 @@
+﻿namespace CP1C
+{
+class Funcionario
+{
+    private string nome;
+
+    public string Nome
+    {
+        get { return Nome; }
+        set { nome = value; }
+    }
+
+
+        public Funcionario(string nome) => Nome = nome;
+
+
+        public virtual void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+    }
+}
+}
