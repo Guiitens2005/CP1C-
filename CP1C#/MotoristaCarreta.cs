503af5fa8e314fa77f987354e5f7810c0a9900b0
Caseiro <guilherme.a.caseiro@gmail.com>
2026-09-08 15:52:08 -0300

    feat: criação da Classe Filha MotoristaCarreta

---
+++ CP1C#/MotoristaCarreta.cs
@@ -0,0 +1,13 @@
+﻿namespace CP1C
+{
+
+    class Motorista : Funcionario
+    {
+        public string TipoVeiculo { get; set; }
+        public Motorista(string nome, string tipoVeiculo) : base(nome) => TipoVeiculo = tipoVeiculo;
+        public override void ExibirInformacoes()
+        {
+            Console.WriteLine($"[MOTORISTA]: {Nome}, Tipo de Veículo: {TipoVeiculo}");
+        }
+    }
+}
